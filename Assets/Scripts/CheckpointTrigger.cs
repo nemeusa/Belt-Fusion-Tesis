60fe114e239d2c9c98331f1626a19e3c309a3f49
nemeusa <nemeusa@gmail.com>
2026-05-29 11:25:59 -0300

    checkpoint good

--- Assets/Scripts/CheckpointTrigger.cs
+++ Assets/Scripts/CheckpointTrigger.cs
@@ -7,14 +7,20 @@ public class CheckpointTrigger : MonoBehaviour
     private bool _activated = false;
     [SerializeField] GameObject _meshOb;
     [SerializeField] Transform respawnPost;
-    [SerializeField] GameObject meshPlatillo;
+    //[SerializeField] GameObject meshPlatillo;
+    [SerializeField] Material paloCheckpointMat;
+    [SerializeField] Material baseCheckpointMat;
+    [SerializeField] MeshRenderer[] palosCheckpointMesh;
+    [SerializeField] MeshRenderer baseCheckpointMesh;
+
     [SerializeField] List<VisualEffect> effects = new List<VisualEffect>();
 
     [SerializeField] Transform robotPosition;
 
     private void Awake()
     {
-        meshPlatillo.SetActive(false);
+        //meshPlatillo.SetActive(false);
+
 
     }
 
@@ -29,8 +35,11 @@ public class CheckpointTrigger : MonoBehaviour
 
     public void CreateCheckpoint()
     {
-        meshPlatillo.SetActive(true);
-        foreach (var p in effects) p.SendEvent("OnPlay");
+        //meshPlatillo.SetActive(true);
+        //foreach (var p in effects) p.SendEvent("OnPlay");
+        baseCheckpointMesh.material = baseCheckpointMat;
+        foreach (var p in palosCheckpointMesh) p.material = paloCheckpointMat;
+
         CheckpointManager.Instance.UpdateCheckpoint(respawnPost.position);
         _activated = true;
 
